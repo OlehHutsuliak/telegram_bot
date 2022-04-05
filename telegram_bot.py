eb3939abb7e03202c112a3def39a0ffd8685131b
OlehHutsuliak <guculoleg24.03@gmail.com>
2022-04-05 08:54:05 +0200

    Add function which returns currency exchange rate

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,23 +5,46 @@ from datetime import date
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import logging
 
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/51.0.2704.103 ""Safari/537.36 "}
+token = 'TOKEN'
 
-def getTimeData():
-    # create url
+
+def get_sun_time_data():
     url = 'https://www.sunrise-and-sunset.com/en/sun/poland/krakow'
-    # get page
-    page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, "
-                                                    "like Gecko) Chrome/51.0.2704.103 ""Safari/537.36 "})
-    html_structure = BeautifulSoup(page.text, 'html.parser')
-    table_with_data = html_structure.table.find_all('td')[2:6]
-    sunrise_time, sunset_time, day_length_time = \
-        table_with_data[0].text, table_with_data[1].text, table_with_data[3].text
+    page = requests.get(url, headers=headers)
+
+    time_data = BeautifulSoup(page.text, 'html.parser').table.find_all('td')[2:6]
+
+    index = 0
+    while index != len(time_data):
+        time_data[index] = time_data[index].text.strip()
+        index += 1
+
+    sunrise_time, sunset_time, timezone, day_length_time = time_data
     today = date.today().strftime('%d %B, %Y')
-    return sunrise_time.strip(), sunset_time.strip(), day_length_time.strip().replace(' ', ':'), today
+
+    return sunrise_time, sunset_time, day_length_time.replace(' ', ':'), today
 
 
-token = 'TELEGRAM TOKEN'
-sunrise, sunset, day_length, today_date = getTimeData()
+def currency_course():
+    amount = "1"
+    sell = ["USD", "EUR"]
+    buy = "PLN"
+    url_usd_pln = f"https://www.xe.com/currencyconverter/convert/?Amount={amount}&From={sell[0]}&To={buy}"
+    url_eur_pln = f"https://www.xe.com/currencyconverter/convert/?Amount={amount}&From={sell[1]}&To={buy}"
+
+    # Get pages
+    currency_pair_one_page = requests.get(url_usd_pln, headers=headers)
+    currency_pair_two_page = requests.get(url_eur_pln, headers=headers)
+
+    currency_pair_one_page_element = BeautifulSoup(currency_pair_one_page.text, 'html.parser').main.find_all('p')[1]
+    currency_pair_two_page_element = BeautifulSoup(currency_pair_two_page.text, 'html.parser').main.find_all('p')[1]
+
+    usd_to_pln = currency_pair_one_page_element.text[:4]
+    eur_to_pln = currency_pair_two_page_element.text[:4]
+
+    return usd_to_pln, eur_to_pln
 
 
 def suntime(update: Update, context: CallbackContext):
@@ -32,6 +55,18 @@ def suntime(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"{text_to_send}")
 
 
+def currency_exchange(update: Update, context: CallbackContext):
+    text_to_send = f"Today God gives us:\n" \
+                   f"        \U0001F928 just \U0001F928\n" \
+                   f"{usd_pln} PLN for 1 USD\n" \
+                   f"{eur_pln} PLN for 1 EUR"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_to_send)
+
+
+sunrise, sunset, day_length, today_date = get_sun_time_data()
+usd_pln, eur_pln = currency_course()
+
+
 def main() -> None:
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
@@ -40,6 +75,7 @@ def main() -> None:
                         level=logging.INFO)
     # Add handlers
     dispatcher.add_handler(CommandHandler('suntime', suntime))
+    dispatcher.add_handler(CommandHandler('currency', currency_exchange))
     # Start - Stop
     updater.start_polling()
     updater.idle()
