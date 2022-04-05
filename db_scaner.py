bfae6b5cede53ef6bca77063ae1234c1b4e23a11
OlehHutsuliak <olegucul@gmail.com>
2022-04-05 16:53:46 +0200

    Add new module that scan our Redis DB

---
+++ db_scaner.py
@@ -0,0 +1,14 @@
+import redis
+import os
+fgfgfgfgfgfgf
+
+redis_url = os.environ["REDIS_URL"]
+redis_connection = redis.from_url(redis_url)
+db_keys = redis_connection.keys(pattern="*")
+
+print((len(db_keys)))
+
+for key in db_keys:
+    chat_id_value = redis_connection.get(key).decode("UTF-8")
+    print(key.decode("UTF-8"), ": ", chat_id_value)
+
