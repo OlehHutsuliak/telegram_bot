9e13c3b4f264e5355da8c0b8156a0b8945093c80
OlehHutsuliak <olegucul@gmail.com>
2022-04-07 14:45:54 +0200

    Refactor code

--- variables.py
+++ variables.py
@@ -20,10 +20,15 @@ def get_sun_time_data():
         time_data[index] = time_data[index].text.strip()
         index += 1
 
-    sunrise_time, sunset_time, timezone, day_length_time = time_data
-    today = date.today().strftime('%d %B, %Y')
+    sunrise, sunset, timezone, day_length = time_data
+    today_date = date.today().strftime('%d %B, %Y')
 
-    return sunrise_time, sunset_time, day_length_time.replace(' ', ':'), today
+    suntime_text = f"\U0001F313 Hi, today is {today_date} \U0001F313\n" \
+                   f"                sunrise - {sunrise}\n" \
+                   f"                 sunset - {sunset}\n" \
+                   f"           day length - {day_length}"
+
+    return suntime_text
 
 
 def currency_course():
@@ -40,27 +45,18 @@ def currency_course():
     currency_pair_one_page_element = BeautifulSoup(currency_pair_one_page.text, 'html.parser').main.find_all('p')[1]
     currency_pair_two_page_element = BeautifulSoup(currency_pair_two_page.text, 'html.parser').main.find_all('p')[1]
 
-    usd_to_pln = currency_pair_one_page_element.text[:4]
-    eur_to_pln = currency_pair_two_page_element.text[:4]
+    usd_pln = currency_pair_one_page_element.text[:4]
+    eur_pln = currency_pair_two_page_element.text[:4]
 
-    return usd_to_pln, eur_to_pln
-
-
-sunrise, sunset, day_length, today_date = get_sun_time_data()
-usd_pln, eur_pln = currency_course()
+    currency_exchange_text = f"   According to the Lord's will\n" \
+                             f"               \U0001F640 \U0001F640 \U0001F640\n" \
+                             f" \U0001F56F {usd_pln} PLN for 1 USD \U0001F56F\n" \
+                             f" \U0001F56F {eur_pln} PLN for 1 EUR \U0001F56F"
+    return currency_exchange_text
 
 
 class Variables:
     token = os.environ["TOKEN"]
     redis_connection = redis.from_url(os.environ["REDIS_URL"])
     bot = telebot.TeleBot(token)
-
-    suntime_text = f"\U0001F313 Hi, today is {today_date} \U0001F313\n" \
-                   f"                sunrise - {sunrise}\n" \
-                   f"                 sunset - {sunset}\n" \
-                   f"           day length - {day_length}"
-    currency_exchange_text = f"   According to the Lord's will\n" \
-                             f"               \U0001F640 \U0001F640 \U0001F640\n" \
-                             f" \U0001F56F {usd_pln} PLN for 1 USD \U0001F56F\n" \
-                             f" \U0001F56F {eur_pln} PLN for 1 EUR \U0001F56F"
     message_filter_text = "This Bot doesn't receive messages. Please select one of available commands below"
